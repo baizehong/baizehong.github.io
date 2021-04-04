ec0bb5ed0c72a16698b288241a80a04b3f13e31a
白泽虹 <57322051+baizehong@users.noreply.github.com>
2021-04-04 02:13:23 -0500

    Create 常用，cookie.js

---
+++ 自用/常用，cookie.js
@@ -0,0 +1,15 @@
+
+function 指定Cookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  //alert(ca[0]);
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+
+
